4b7e1e0be1e1be6dfadccd45cf74472737565d4d
David Lee <Dai180293@gmail.com>
2024-03-13 10:02:35 +0000

    FIRST COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+    return <>portfolio</>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                csBlack: "hsl(45,6,13)",
+                csWhite: "hsl(0,0,88)",
+                csGrey: "hsl(0,0,65)",
+                csAccent: "hsl(40,78,61)",
+            },
+            fontFamily: {
+                Manrope: ["Manrope", "sans-serif"],
+            },
+            fontSize: {
+                sm: "var(--font-size-sm)",
+                base: "var(--font-size-base)",
+                lg: "var(--font-size-lg)",
+                xl: "var(--font-size-xl)",
+                "2xl": "var(--font-size-2xl)",
+                "3xl": "var(--font-size-3xl)",
+                "4xl": "var(--font-size-4xl)",
+                "5xl": "var(--font-size-5xl)",
+            },
+        },
+    },
+    plugins: [],
+};
