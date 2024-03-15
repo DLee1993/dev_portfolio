ea5028be3e6fe4d788b91b09c21d5b63fc7cff41
David Lee <Dai180293@gmail.com>
2024-03-15 11:43:52 +0000

    removed loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import Hero from "./components/Hero";
 import SelectedWorks from "./components/SelectedWorks";
 import Contact from "./components/Contact";
 import Menu from "./components/Menu";
-import Loader from "./components/Loader";
 function App() {
     return (
         <>
             <Menu />
-            <Loader />
             <Header />
             <main>
                 <Hero />
